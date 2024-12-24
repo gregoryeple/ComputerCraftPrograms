1f9ecb067420d4f388f5ecfae301c9c8c65e853b
Grégory EPLE <gregory.eple@epitech.eu>
2024-12-24 11:00:12 +0100

    Added user interface to peripheralDetector.lua

--- peripheralDetector.lua
+++ peripheralDetector.lua
@@ -3,7 +3,7 @@
 PeripheralDetector program
 By Out-Feu
 
-version 1.1.0
+version 2.0.0
 
 Free to distribute/alter
 so long as proper credit to original
@@ -59,10 +59,15 @@ function findPeripherals()
  end
 end
 
+--------------------------------------------------------------------------------
+
+graphicInterface = true -- Should the program display its graphical interface instead of the standard output
 displayMethods = true -- Should all peripherals' methods be printed
 exportFile = "" -- If set, output will be exported to a file (override content)
 sides = {...}
 peripherals = {}
+
+--------------------------------------------------------------------------------
 
 findPeripherals()
 
@@ -70,15 +75,130 @@ if peripherals == nil or next(peripherals) == nil then
  return -- exit from the script if no peripherals were found
 end
 
-if exportFile == nil or exportFile == '' then
- for i, peripheral in pairs(peripherals) do
-  printPeripheral(peripheral, nil)
- end
-else
+if exportFile ~= nil and exportFile ~= "" then
  local file = fs.open(exportFile, "w")
  for i, peripheral in pairs(peripherals) do
   printPeripheral(peripheral, file)
  end
  file.close()
-  print("Exported " .. #peripherals .. " peripherals to " .. exportFile)
+ print("Exported " .. #peripherals .. " peripherals to " .. exportFile)
+elseif not graphicInterface then
+ for i, peripheral in pairs(peripherals) do
+  printPeripheral(peripheral, nil)
+ end
+end
+
+if graphicInterface then
+ local width, height = term.getSize()
+ local usableHeight = height - 5
+ local currentSide = ""
+ local currentMethods = nil
+ local selectedIndex = 1
+ local scrollIndex = 0
+ repeat --main loop
+  term.clear()
+
+  -- Print title and help
+  term.setCursorPos(1, 1)
+  if currentSide == "" then
+   term.write("Found " .. #peripherals .. " connected peripherals")
+  else
+   term.write(currentSide .. ": " .. peripheral.getType(currentSide) .. " (" .. #currentMethods .. " methods)")
+  end
+  term.setCursorPos(1, 2)
+  term.write(string.rep("=", width))
+  if currentSide == "" then
+   term.setCursorPos(1, height - 2)
+   term.write(string.rep("=", width))
+   term.setCursorPos(1, height - 1)
+   term.write("ARROW KEY to move")
+   term.setCursorPos(1, height)
+   term.write("ENTER or SPACE to select")
+  elseif currentMethods ~= nil and #currentMethods > usableHeight then
+   term.setCursorPos(1, height - 2)
+   term.write(string.rep("=", width))
+   term.setCursorPos(1, height - 1)
+   term.write("ARROW KEY to move")
+   term.setCursorPos(1, height)
+   term.write("SHIFT or BACKPSPACE to go back")
+  else
+   term.setCursorPos(1, height - 1)
+   term.write(string.rep("=", width))
+   term.setCursorPos(1, height)
+   term.write("SHIFT or BACKPSPACE to go back")
+  end
+  
+  if currentSide == "" then
+  -- Print peripherals
+   for n = 1 + scrollIndex, usableHeight + scrollIndex do
+    if n > #peripherals then
+	 break
+	end
+    term.setCursorPos(1, 2 + n - scrollIndex)
+	if n == selectedIndex then
+     term.write(" > ")
+    else
+     term.write(" - ")
+	end
+	term.write(peripherals[n] .. ": " .. peripheral.getType(peripherals[n]))
+   end
+  else
+  -- Print methods
+   for n = 1 + scrollIndex, usableHeight + scrollIndex do
+    if n > #currentMethods then
+	 break
+	end
+    term.setCursorPos(1, 2 + n - scrollIndex)
+	term.write(" - " .. currentMethods[n])
+   end
+  end
+
+  -- Wait for key press
+  local event, key = os.pullEvent("key")
+  if key == keys.up or key == keys.pageUp then -- UP ARROW or PAGE UP was pressed
+   if currentSide == "" then
+    if selectedIndex > 1 then
+     selectedIndex = selectedIndex - 1
+	else
+     selectedIndex = #peripherals
+	end
+   elseif currentSide ~= "" and currentMethods ~= nil and scrollIndex > 1 then
+    scrollIndex = scrollIndex - 1
+   end
+  elseif key == keys.down or key == keys.pageDown then -- DOWN ARROW or PAGE DOWN was pressed
+   if currentSide == "" then
+    if selectedIndex < #peripherals then
+     selectedIndex = selectedIndex + 1
+	else
+     selectedIndex = 1
+	end
+   elseif currentSide ~= "" and currentMethods ~= nil and (scrollIndex + usableHeight) < #currentMethods then
+    scrollIndex = scrollIndex + 1
+   end
+  elseif (key == keys.enter or key == keys.space or key == keys.insert) and currentSide == "" then -- ENTER or SPACE or INSERT was pressed
+   currentSide = peripherals[selectedIndex]
+   currentMethods = peripheral.getMethods(currentSide)
+   scrollIndex = 0
+   usableHeight = height - 4
+   if #currentMethods > usableHeight then
+    usableHeight = height - 5
+   end
+  elseif (key == keys.leftShift or key == keys.rightShift or key == keys.backspace or key == keys.delete) and currentSide ~= "" then -- SHIFT or BACKSPACE or DELETE was pressed
+   currentSide = ""
+   currentMethods = nil
+   scrollIndex = 0
+   usableHeight = height - 5
+  end
+
+  -- Handle scrolling
+  if currentSide == "" then
+   if selectedIndex > usableHeight + scrollIndex then
+    scrollIndex = selectedIndex - usableHeight
+   elseif selectedIndex <= scrollIndex then
+    scrollIndex = selectedIndex - 1
+   end
+  end
+ until key == keys.t
+ term.clear()
+ term.setCursorPos(1, 1)
 end
